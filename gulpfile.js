24bb4473c0927002f3c75a53d7ccedf50c8eee60
Marcia Ibanez <marcia.ibanez.1@gmail.com>
2018-03-12 12:26:37 -0300

    adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
-var sass         = require('gulp-sass');
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 var coffeescript = require('gulp-coffeescript');
-var gutil        = require('gulp-util');
-var mustache     = require("gulp-mustache");
+var gutil = require('gulp-util');
+var mustache = require("gulp-mustache");
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', function () {
 
     browserSync.init({
         server: "./"
@@ -22,27 +32,69 @@ gulp.task('serve', function() {
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp
-      .src("scss/style.scss")
-      .pipe(sass())
-      .pipe(gulp.dest("css"))
-      .pipe(browserSync.stream());
+        .src("scss/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('coffee', function() {
-      gulp
+gulp.task('coffee', function () {
+    gulp
         .src("./src/*.coffee")
-        .pipe(coffeescript({ bare: true }).on("error", gutil.log))
+        .pipe(coffeescript({
+            bare: true
+        }).on("error", gutil.log))
         .pipe(gulp.dest("./js/"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('mustache', function() {
+gulp.task('mustache', function () {
     gulp
-      .src("./templates/*.mustache")
-      .pipe(mustache("settings.json", { extension: ".html" }, {}))
-      .pipe(gulp.dest("./"))
-      .pipe(browserSync.stream());
+        .src("./templates/*.mustache")
+        .pipe(mustache("settings.json", {
+            extension: ".html"
+        }, {}))
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
 })
 gulp.task('default', ['serve']);
+
+gulp.task('default', function () {
+    gulp.src('src/css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', () =>
+    gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('scripts', function () {
+    return gulp.src('./lib/script.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('compress', function (cb) {
+    pump([
+        gulp.src('lib/script.js'),
+        uglify(),
+        gulp.dest('dist')
+    ],
+        cb
+    );
+});
+
+gulp.task('default', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
