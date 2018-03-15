e90c928f4e22ed0080babc6463183badea220b53
Marcia Ibanez <marcia.ibanez.1@gmail.com>
2018-03-15 11:45:35 -0300

    adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,8 @@ var coffeescript = require('gulp-coffeescript');
 var gutil = require('gulp-util');
 var mustache = require("gulp-mustache");
 var concat = require('gulp-concat');
-var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
-const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
 // Static Server + watching scss/html files
@@ -26,9 +22,9 @@ gulp.task('serve', function () {
     gulp.watch("src/*.coffee", ['coffee']);
     gulp.watch("templates/*.mustache", ['mustache']);
     gulp.watch("settings.json", ['mustache']);
-
     gulp.watch("*.html").on('change', browserSync.reload);
     gulp.watch("settings.json").on('change', browserSync.reload);
+    
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -36,6 +32,11 @@ gulp.task('sass', function () {
     return gulp
         .src("scss/style.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssmin())
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
@@ -46,6 +47,7 @@ gulp.task('coffee', function () {
         .pipe(coffeescript({
             bare: true
         }).on("error", gutil.log))
+        .pipe(uglify())
         .pipe(gulp.dest("./js/"))
         .pipe(browserSync.stream());
 });
@@ -59,42 +61,10 @@ gulp.task('mustache', function () {
         .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
 })
+gulp.task('imagemin', () =>
+    gulp.src('img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('img/'))
+);
+
 gulp.task('default', ['serve']);
-
-gulp.task('default', function () {
-    gulp.src('src/css/*.css')
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', () =>
-    gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'))
-);
-
-gulp.task('scripts', function () {
-    return gulp.src('./lib/script.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('compress', function (cb) {
-    pump([
-        gulp.src('lib/script.js'),
-        uglify(),
-        gulp.dest('dist')
-    ],
-        cb
-    );
-});
-
-gulp.task('default', () =>
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
-);
